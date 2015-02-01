24a26504d1a3d444004cf6460ea52da09dcd8078
slee-colonel <slee_colonel@hotmail.com>
2015-01-31 20:01:43 -0800

    Remapped "show/3" to "first::gimme/3" using wildcards

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -39,4 +39,19 @@ class First extends Application {
         // render the page
         $this->render();
     }
+    
+    function gimme() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        
+        // call the function to grab the first quote data
+        $record = $this->quotes->get('3');
+        
+        // match up the data required by the justone page to the data given to us
+        $this->data['mug'] = $record['mug'];
+        $this->data['who'] = $record['who'];
+        $this->data['what'] = $record['what'];
+        
+        // render the page
+        $this->render();
+    }
 }
