cf54ea17d521b405c27d5fee53730b7f0678ef9c
slee-colonel <slee_colonel@hotmail.com>
2015-01-31 18:59:45 -0800

    Got the "First" link fixed.

---
+++ application/controllers/First.php
@@ -0,0 +1,25 @@
+<?php
+
+class First extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // build the list of authors, to pass on to our view
+        $record = $this->quotes->first();
+        
+        $this->data['mug'] = $record['mug'];
+        $this->data['who'] = $record['who'];
+        $this->data['what'] = $record['what'];
+        
+        $this->render();
+    }
+
+}
